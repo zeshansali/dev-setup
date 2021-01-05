a755c1a748c7d21c2ba5301d8c937b1878132cc9
Zeshan S. Ali <zeshansali@gmail.com>
2021-01-05 11:33:57 -0600

    dev: create setup script

---
+++ setup.sh
@@ -0,0 +1,39 @@
+# generate ssh key for new device
+# todo: get private key name from first command or pass
+#      in as command line param
+ssh-keygen -t ed25519 -C "email-address"
+eval "$(ssh-agent -s)"
+ssh-add -K ~/path/to/private/key/file
+
+# install command line tools
+# todo: there may be some errors that need to be ironed out here
+xcode-select --install
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install tech
+brew install git
+brew install postgresql
+brew install tmux
+
+# sdks
+curl -s "https://get.sdkman.io" | bash
+sdk i java 8.0.275.j9-adpt # may be missing 
+sdk i java 11.0.9.j9-adpt
+n # don't set to default
+sdk i scala 2.13.4
+sdk u scala 2.13.4
+
+# clone dev-setup
+git clone git@github.com:zeshansali/dev-setup.git ~/setup
+
+# install oh-my-zsh
+sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# setup dot file symlinks
+dotFiles=(.zshrc .vimrc .gitconfig .gitignore)
+for dotFile in ${dotFiles[@]}; do
+  rm $dotFile
+  ln -s ~/setup/dot-files/$dotFile ~/$dotFile
+done
